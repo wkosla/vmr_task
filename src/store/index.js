61a8e88b61bf5f7af9415e1764979cae5101ffc0
Wojciech Kosla <w.kosla@gmail.com>
2020-02-04 19:57:51 +0100

    Users list with pagination complete

--- src/store/index.js
+++ src/store/index.js
@@ -23,13 +23,17 @@ export default new Vuex.Store({
   state: {
     users: [],
     pages: 0,
-    currPage: 0,
+    currPage: 1,
+    perPage: 0,
+    total: 0,
     userDetails: {},
   },
   getters: {
     getUsers: state => state.users,
     getPages: state => state.pages,
     getCurrPage: state => state.currPage,
+    getPerPage: state => state.perPage,
+    getTotal: state => state.total,
     getCurrUser: state => state.userDetails,
   },
   mutations: {
@@ -45,6 +49,12 @@ export default new Vuex.Store({
     SET_USER: (state, user) => {
       state.userDetails = user;
     },
+    SET_PER_PAGE: (state, num) => {
+      state.perPage = num;
+    },
+    SET_TOTAL: (state, num) => {
+      state.total = num;
+    },
   },
   actions: {
     GET_USERS: ({ commit }, page) => {
@@ -52,6 +62,8 @@ export default new Vuex.Store({
         commit('SET_USERS', data.data);
         commit('SET_PAGES', data.total);
         commit('SET_CURRENT_PAGE', data.page);
+        commit('SET_PER_PAGE', data.per_page);
+        commit('SET_TOTAL', data.total);        
       });
     },
     GET_USER: ({ commit }, id) => {
--- src/views/Users.vue
+++ src/views/Users.vue
@@ -1,13 +1,68 @@
 <template>
-  <div>
+  <div id="users">
+    <b-table hover :items="users" :fields="fields">
+      <template v-slot:cell(avatar)="data">
+        <b-img rounded fluid :src="data.item.avatar"></b-img>
+      </template>
+      <template v-slot:cell(details)>
+        Details
+      </template>
+    </b-table>
+    <b-pagination
+      v-model="currPage"
+      :total-rows="total"
+      :per-page="perPage"
+    ></b-pagination>
   </div>
 </template>
 
 <script>
 export default {
   name: 'users',
+  data: () => ({
+    fields: [
+      'id',
+      {
+        key: 'avatar', label: 'Avatar',
+      },
+      'first_name',
+      'last_name',
+      'email',
+      {
+        key: 'details', label: '',
+      }
+    ],
+  }),
+  computed: {
+    users() {
+      return this.$store.getters.getUsers;
+    },
+    currPage: {
+      get() {
+        return this.$store.getters.getCurrPage;
+      },
+      set(page) {
+        this.$store.dispatch('GET_USERS', page);
+      },
+    },
+    pages() {
+      return this.$store.getters.getPages;
+    },
+    perPage() {
+      return this.$store.getters.getPerPage;
+    },
+    total() {
+      return this.$store.getters.getTotal;
+    },
+  },
   mounted() {
     this.$store.dispatch('GET_USERS');
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.img-fluid {
+  max-width: 36px;
+}
+</style>
